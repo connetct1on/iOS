c62f42aa5e1ae2813b690296e493aca82676227e
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2022-11-24 20:12:17 +0900

    ✨:: Navigation Bar Item 추가

--- connect1on/connect1on/New Group/Home.swift
+++ connect1on/connect1on/New Group/Home.swift
@@ -12,17 +12,36 @@ import SnapKit
 
 class HomeVC: UIViewController {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        setupNavigationBarItem()
+        self.navigationController?.navigationBar.tintColor = .black
     }
     func setupNavigationBar() {
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.backgroundColor = .systemBackground
         navigationController?.navigationBar.standardAppearance = navigationBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
-//        @objc func buttonPressed() {
-//            print("검색")
-//        }
+    }
+   
+        func setupNavigationBarItem() {
+            
+            let massageButton = UIBarButtonItem(
+                image: UIImage(systemName: "paperplane"),
+                style: .plain,
+                target: self,
+                action: #selector(didTabMassageButton)
+            )
+            
+            navigationItem.rightBarButtonItem = massageButton
+        }
+    @objc func didTabMassageButton() {
+        let VC = MassageVC()
+        present(VC, animated: true, completion: nil)
+        UIView.animate(withDuration: 0.3) {
+            self.view.window?.rootViewController = MassageVC()
+        }
     }
 }
---
+++ connect1on/connect1on/New Group/Massage.swift
@@ -0,0 +1,19 @@
+//
+//  Massage.swift
+//  connect1on
+//
+//  Created by 최시훈 on 2022/11/24.
+//
+
+import Foundation
+import SnapKit
+import Then
+import UIKit
+
+class MassageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
