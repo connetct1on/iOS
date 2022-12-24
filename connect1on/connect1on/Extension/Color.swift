0342632fe447b91a88e2fcc0e6370c344a1eac7d
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2022-12-24 13:23:41 +0900

    ✨ :: 오토레이아웃 다시 잡음

--- connect1on/connect1on/Extension/Color.swift
+++ connect1on/connect1on/Extension/Color.swift
@@ -8,8 +8,7 @@
 import UIKit
 
 extension UIColor {
-    
     static var mainColor = UIColor(red: 10.0 / 255, green: 0.0 / 255, blue: 114.0 / 255, alpha: 1)
-    
+    static var secondColor = UIColor(red: 220.0/255, green: 220.0/255, blue: 220.0/255, alpha: 1)
 }
 
--- connect1on/connect1on/login/signin.swift
+++ connect1on/connect1on/login/signin.swift
@@ -24,18 +24,16 @@ class SigninVC: UIViewController {
         $0.font = UIFont(name: "GangwonEduAll-OTFBold", size:150)
         $0.font.withSize(58)
         $0.textAlignment = .center //가운데 정렬
-        //$0.backgroundColor = UIColor(red: 180.0 / 255, green: 180.0 / 255, blue: 180.0 / 255, alpha: 1)
     }
     let idlb = UILabel().then {
         $0.font = UIFont(name: "GangwonEduAll-OTFBold", size: 20)
         $0.text = " ID"
-        //$0.backgroundColor = UIColor(red: 220.0 / 255, green: 220.0 / 225, blue: 220.0 / 225, alpha: 0.3)
     }
     lazy var idTextField = UITextField().then {
         $0.placeholder = "  아이디를 입력해주세요"
         $0.font = .systemFont(ofSize: 14.0, weight: .medium)
         $0.autocapitalizationType = .none
-        $0.backgroundColor = UIColor(red: 220.0/255, green: 220.0/255, blue: 220.0/255, alpha: 1)
+        $0.backgroundColor = .secondColor
         $0.layer.cornerRadius = 20
     }
     let pwlb = UILabel().then {
@@ -47,11 +45,11 @@ class SigninVC: UIViewController {
         $0.font = .systemFont(ofSize: 14.0, weight: .medium)
         $0.autocapitalizationType = .none
         $0.isSecureTextEntry = true
-        $0.backgroundColor = UIColor(red: 220.0/255, green: 220.0/255, blue: 220.0/255, alpha: 1)
+        $0.backgroundColor = .secondColor
         $0.layer.cornerRadius = 20
     }
     let signinBt = UIButton().then {
-        $0.backgroundColor = UIColor(red:  10.0/255, green:0.0/255, blue:114.0/255, alpha: 1)
+        $0.backgroundColor = .mainColor
         $0.setTitle("로그인", for: .normal)
         $0.layer.cornerRadius = 20
         $0.addTarget(self, action: #selector(TabsigninBt), for: .touchUpInside)
@@ -98,20 +96,19 @@ extension SigninVC {
         ].forEach{ self.view.addSubview($0)
         }
         logolb.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(150)
-            $0.bottom.equalToSuperview().offset(-580)
+            $0.top.equalToSuperview().offset(120)
+            $0.bottom.equalTo(idTextField.snp.top).offset(-50)
             $0.left.equalToSuperview().offset(70)
             $0.right.equalToSuperview().offset(-70)
         }
         idlb.snp.makeConstraints {
-            $0.top.equalTo(idTextField).offset(-25)
-            $0.bottom.equalTo(idTextField).offset(-50)
+            $0.top.equalTo(idTextField.snp.top).offset(-25)
+            $0.bottom.equalTo(idTextField.snp.top).offset(-10)
             $0.left.equalToSuperview().offset(70)
             $0.right.equalToSuperview().offset(-70)
         }
         idTextField.snp.makeConstraints {
-            $0.top.equalTo(logolb).offset(150)
-            $0.bottom.equalTo(logolb).offset(90)
+            $0.bottom.equalTo(logolb.snp.bottom).offset(100)
             $0.left.equalToSuperview().offset(70)
             $0.right.equalToSuperview().offset(-70)
         }
@@ -122,14 +119,14 @@ extension SigninVC {
             $0.right.equalToSuperview().offset(-70)
         }
         pwTextField.snp.makeConstraints {
-            $0.top.equalTo(logolb).offset(230)
-            $0.bottom.equalTo(logolb).offset(170)
+            $0.top.equalTo(logolb.snp.bottom).offset(130)
+            $0.bottom.equalTo(logolb.snp.bottom).offset(180)
             $0.left.equalToSuperview().offset(70)
             $0.right.equalToSuperview().offset(-70)
         }
         signinBt.snp.makeConstraints {
-            $0.top.equalTo(logolb).offset(310)
-            $0.bottom.equalTo(logolb).offset(250)
+            $0.top.equalTo(pwTextField.snp.bottom).offset(20)
+            $0.bottom.equalTo(pwTextField.snp.bottom).offset(70)
             $0.left.equalToSuperview().offset(70)
             $0.right.equalToSuperview().offset(-70)
         }
