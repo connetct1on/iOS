c586b936c2f018dc7d206d92c9e1a6398b2669c0
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-01-02 18:06:14 +0900

    ✨ :: FacilitiesVC pagecontrol 완성

--- connect1on/connect1on/TabBar/Home/FacilitiesVC.swift
+++ connect1on/connect1on/TabBar/Home/FacilitiesVC.swift
@@ -10,7 +10,7 @@ import Then
 import SnapKit
 
 
-class FacilitiesVC: UIViewController {
+class FacilitiesVC: UIViewController, UIScrollViewDelegate {
     let navigationBar = UINavigationBar().then {
         $0.backgroundColor = UIColor(red: 230.0/255, green: 230.0/255, blue: 230.0/255, alpha: 1)
     }
@@ -26,22 +26,61 @@ class FacilitiesVC: UIViewController {
         $0.textAlignment = .center //가운데 정렬
         
     }
-    let image = UIImageView().then {
-        $0.image = UIImage(named: "ground")
-        
+    var imageNames = ["ground", "love", "dgsw_logo"]
+    let pageControl = UIPageControl().then {
+        $0.currentPage = 3
+        $0.numberOfPages = 3
+        $0.pageIndicatorTintColor = .lightGray
+        $0.currentPageIndicatorTintColor = .black
+    }
+    let scrollView = UIScrollView().then {
+        $0.frame = UIScreen.main.bounds
+        $0.alwaysBounceVertical = false
+        $0.showsHorizontalScrollIndicator = false
+        $0.showsVerticalScrollIndicator = false
+        $0.isScrollEnabled = true
+        $0.isPagingEnabled = true
+        $0.bounces = false
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setup()
-        
+        //        setupNavigationBarItem()
+        scrollView.delegate = self
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width * CGFloat(imageNames.count), height: UIScreen.main.bounds.height)
+        insertImageIntoScrollView()
     }
 }
 extension FacilitiesVC {
+    func insertImageIntoScrollView() {
+        for (index, imageName) in imageNames.enumerated() {
+            let image = UIImage(named: imageName)
+            let imageView = UIImageView(image: image)
+            imageView.frame = UIScreen.main.bounds
+            imageView.frame.origin.x = UIScreen.main.bounds.width * CGFloat(index)
+            scrollView.addSubview(imageView)
+            
+        }
+    }
+    //    func setupNavigationBarItem() {
+    //            image: UIImage(systemName: "paperplane"),
+    //            style: .plain,
+    //            target: self,
+    //            action: #selector(didTapBackButton)
+    //        )
+    //        navigationItem.leftBarButtonItem = backButton
+    //    }
+    @objc func didTapBackButton() {
+        self.dismiss(animated: true)
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageControl.currentPage = Int(floor(scrollView.contentOffset.x / UIScreen.main.bounds.width))
+    }
 func setup() {
     [
-        navigationBar, line, explanation, image
+        navigationBar, line, explanation, scrollView, pageControl
     ].forEach{ self.view.addSubview($0) }
     navigationBar.snp.makeConstraints {
         $0.top.equalToSuperview().offset(0)
@@ -57,15 +96,20 @@ func setup() {
     }
     explanation.snp.makeConstraints {
         $0.top.equalToSuperview().offset(100)
-        $0.bottom.equalTo(image.snp.top).offset(-5)
+        $0.bottom.equalTo(scrollView.snp.top).offset(-5)
         $0.left.equalToSuperview().offset(0)
         $0.right.equalToSuperview().offset(0)
     }
-    image.snp.makeConstraints {
+    scrollView.snp.makeConstraints {
         $0.top.equalToSuperview().offset(170)
         $0.bottom.equalToSuperview().offset(-270)
         $0.left.equalToSuperview().offset(0)
         $0.right.equalToSuperview().offset(0)
+    }
+    pageControl.snp.makeConstraints {
+        $0.bottom.equalTo(scrollView.snp.bottom).offset(-10)
+        $0.left.equalTo(scrollView.snp.left).offset(0)
+        $0.right.equalTo(scrollView.snp.right).offset(0)
     }
     
 }
