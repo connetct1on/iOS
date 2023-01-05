8494c2209c3e11c9518498029de02a583470f6f0
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-01-05 20:47:21 +0900

    ✨ :: Add QuestionVC NavigationBar

--- connect1on/connect1on/TabBar/QuestionVC.swift
+++ connect1on/connect1on/TabBar/QuestionVC.swift
@@ -12,6 +12,17 @@ class QuestionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+        view.backgroundColor = .white
     }
+    
 
+}
+extension QuestionVC {
+    private func setupNavigationBar() {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = UIColor(red: 10.0/255, green:0.0/255, blue:114.0/255, alpha: 1)
+        navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+    }
 }
