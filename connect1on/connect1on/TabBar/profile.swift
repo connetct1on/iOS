7c04d88d433f94f69dc6b0298557aef3cd32ad82
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-01-09 01:28:35 +0900

    ✨ :: ProfileVC 코드 안정성 높임, 코드 줄 정리

--- connect1on/connect1on/TabBar/profile.swift
+++ connect1on/connect1on/TabBar/profile.swift
@@ -21,16 +21,12 @@ class ProfileVC: UIViewController {
     }
 }
 extension ProfileVC {
-    
     private func setupNavigationBar() {
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.backgroundColor = UIColor(red: 10.0/255, green:0.0/255, blue:114.0/255, alpha: 1)
         navigationController?.navigationBar.standardAppearance = navigationBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
-        
     }
-    
-
 private func setupNavigationBarItem() {
     let setButton = UIBarButtonItem(
         image: UIImage(systemName: "gearshape.fill"),
@@ -38,13 +34,10 @@ private func setupNavigationBarItem() {
         target: self,
         action: #selector(didTabSetButton))
     navigationItem.rightBarButtonItem = setButton
-    
 }
     @objc func didTabSetButton() {
-        print("didTabSetButton")
         let VC = SetVC()
         VC.hidesBottomBarWhenPushed = true
-        self.navigationController?.pushViewController(VC, animated: true)
-        
+        navigationController?.pushViewController(VC, animated: true)
     }
 }
