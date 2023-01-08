e0fcc0ce645e836e8cff111d78acb0864dbb3344
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-01-09 01:35:19 +0900

    ✨ :: 코드 줄 정리 tableView 추가

--- connect1on/connect1on/TabBar/QuestionVC.swift
+++ connect1on/connect1on/TabBar/QuestionVC.swift
@@ -7,22 +7,58 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 class QuestionVC: UIViewController {
-
+    let tableView = UITableView().then {
+        $0.register(QuestionTableViewCell.self, forCellReuseIdentifier: QuestionTableViewCell.className)
+//        $0.delegate = self
+//        $0.dataSource = self
+    }
+    
+    var post = [QuestionTableView]()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
         view.backgroundColor = .white
     }
-    
-
 }
 extension QuestionVC {
+    func setup() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
     private func setupNavigationBar() {
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.backgroundColor = UIColor(red: 10.0/255, green:0.0/255, blue:114.0/255, alpha: 1)
         navigationController?.navigationBar.standardAppearance = navigationBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+    }
+    private func setupNavigationBarItem() {
+        let AddBt = UIBarButtonItem(
+            image: UIImage(systemName: "plus"),
+            style: .plain,
+            target: self,
+            action: #selector(didTabAddBt)
+        )
+    }
+    @objc func didTabAddBt() {
+        let VC = AddVC()
+        let navigationController = UINavigationController(rootViewController: VC)
+        present(navigationController, animated: true, completion: nil)
+    }
+    
+}
+extension QuestionVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.post.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
     }
 }
