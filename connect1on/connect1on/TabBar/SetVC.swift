0671b639326c339f3ddef2737e640d0437f97e60
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-01-09 01:30:27 +0900

    ✨ :: 쓸모없는 코드, 쓸모없는 주석 삭제

--- connect1on/connect1on/TabBar/SetVC.swift
+++ connect1on/connect1on/TabBar/SetVC.swift
@@ -6,21 +6,29 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+
 
 class SetVC: UIViewController {
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        self.navigationController?.navigationBar.tintColor = .white
-        setupNavigationItem()
         
     }
 }
 extension SetVC {
-    private func setupNavigationItem() {
-        navigationItem.title = "설정"
-        navigationController?.navigationBar.titleTextAttributes = [. foregroundColor: UIColor.white]
-    }
+//    func setup() {
+//        [
+//            tableView
+//        ].forEach{ self.view.addSubview($0) }
+//        tableView.snp.makeConstraints {
+//            $0.top.equalToSuperview().offset(100)
+//            $0.bottom.equalToSuperview().offset(-670)
+//            $0.left.equalToSuperview().offset(40)
+//            $0.right.equalToSuperview().offset(-40)
+//
+//        }
+//    }
 }
