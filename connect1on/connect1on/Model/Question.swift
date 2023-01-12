c2015fd8714b90f0206638df65c7161d284b8b90
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-01-12 22:55:38 +0900

    ✨ :: 일단 테스트로 변수 설정해놓고 나중에 바꿀거

--- connect1on/connect1on/Model/Question.swift
+++ connect1on/connect1on/Model/Question.swift
@@ -7,14 +7,19 @@
 
 import UIKit
 
-struct QuestionTableView {
-    var profileImage: UIImage?
-    var studentNumber: Int?
-    var name: String?
-    var title: String?
-    var detail: String?
+struct PostDatas {
+//    var profileImage: UIImage(named: "love")
+    var profileImage = UIImage(named: "love")
+//    var studentNumber: String?
+    var studentNumber = "1117"
+//    var name: String?
+    var name = "최시훈"
+//    var title: String?
+    var title = "화장실에..."
+//    var detail: String?
+    var detail = "화장실에 쓸 화장지 필요한가요?"
     
-    init(profileImage : UIImage, studentNumber : Int, name: String, title: String, detail: String) {
+    init(profileImage : UIImage, studentNumber : String, name: String, title: String, detail: String) {
         self.profileImage = profileImage
         self.studentNumber = studentNumber
         self.name = name
