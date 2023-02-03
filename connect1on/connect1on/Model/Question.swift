4f4353f5d38aa5bf03244ac60a249ee10001ed16
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-02-03 23:13:37 +0900

    ✨ :: Kingfisher쓸거라 UIImage String으로 바꿈

--- connect1on/connect1on/Model/Question.swift
+++ connect1on/connect1on/Model/Question.swift
@@ -8,18 +8,13 @@
 import UIKit
 
 struct PostDatas {
-//    var profileImage: UIImage(named: "love")
-    var profileImage = UIImage(named: "love")
-//    var studentNumber: String?
-    var studentNumber = "1117"
-//    var name: String?
-    var name = "최시훈"
-//    var title: String?
-    var title = "화장실에..."
-//    var detail: String?
-    var detail = "화장실에 쓸 화장지 필요한가요?"
+    var profileImage: String?
+    var studentNumber: String?
+    var name: String?
+    var title: String?
+    var detail: String?
     
-    init(profileImage : UIImage, studentNumber : String, name: String, title: String, detail: String) {
+    init(profileImage: String, studentNumber: String, name: String, title: String, detail: String) {
         self.profileImage = profileImage
         self.studentNumber = studentNumber
         self.name = name
