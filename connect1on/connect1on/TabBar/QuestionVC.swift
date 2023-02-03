84a64d75fb6634a74ee162355301ffbac8c6aee9
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-02-03 23:14:32 +0900

    ✨ :: Kingfisher공부하고 마저 할 것

--- connect1on/connect1on/TabBar/QuestionVC.swift
+++ connect1on/connect1on/TabBar/QuestionVC.swift
@@ -9,65 +9,97 @@
 import UIKit
 import Then
 import SnapKit
+import Kingfisher
 
-class QuestionVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var postDatas = [PostDatas]()
-    let tableView = UITableView().then {
-        $0.register(QuestionTableViewCell.self, forCellReuseIdentifier: QuestionTableViewCell.className)
+class QuestionVC: UIViewController {
+    var datas: [PostDatas] = [
+        PostDatas(profileImage: "as", studentNumber: "asdf1", name: "asdf1", title: "asdf1", detail: "asdf1")
+    ]
+    let url = URL(string: "https://ifh.cc/g/J8wf2m.png")
+    
+    let tableView = UITableView(frame: .zero).then {
+        $0.backgroundColor = .white
+        $0.register(QuestionTableViewCell.self, forCellReuseIdentifier: "QuestionTableViewCell")
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.reloadData()
+        setupTableView()
         setupNavigationBar()
         setupNavigationBarItem()
+        configureTableView()
+        setup()
+        self.tableView.reloadData()
         view.backgroundColor = .white
-        tableView.dataSource = self
-        tableView.delegate = self
     }
+    
+    func setupTableView(){
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints{ $0.edges.equalToSuperview()}
+    }
+
 }
 extension QuestionVC {
-    private func setup() {
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
+    func configureTableView() {
+        tableView.dataSource = self
     }
-    private func setupNavigationBar() {
+    
+    func setup() {
+        [
+            tableView
+        ].forEach{ self.view.addSubview($0)
+        }
+        tableView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().offset(90)
+            $0.left.right.equalToSuperview().offset(0)
+            
+        }
+        
+    }
+    
+    func setupNavigationBar() {
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.backgroundColor = UIColor(red: 10.0/255, green:0.0/255, blue:114.0/255, alpha: 1)
         navigationController?.navigationBar.standardAppearance = navigationBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
     }
-    private func setupNavigationBarItem() {
+    
+    func setupNavigationBarItem() {
         let AddBt = UIBarButtonItem(
             image: UIImage(systemName: "plus"),
             style: .plain,
             target: self,
             action: #selector(didTabAddBt)
         )
+        navigationItem.title = "love"
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.titleView?.tintColor = .white
         navigationItem.rightBarButtonItem = AddBt
     }
+    
     @objc func didTabAddBt() {
         let VC = AddVC()
         let navigationController = UINavigationController(rootViewController: VC)
         present(navigationController, animated: true, completion: nil)
     }
-    
 }
-extension QuestionVC {
-
+extension QuestionVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.postDatas.count
+        return datas.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: QuestionTableViewCell.className, for: indexPath) as! QuestionTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionTableViewCell", for: indexPath) as? QuestionTableViewCell
         
-        cell.profileImageView.image = self.postDatas[indexPath.section].profileImage
-        cell.studentNumber.text = self.postDatas[indexPath.section].studentNumber
-        cell.name.text = self.postDatas[indexPath.section].name
-        cell.title.text = self.postDatas[indexPath.section].title
-        cell.detail.text = self.postDatas[indexPath.section].detail
+        cell!.profileImageView.text = self.datas[indexPath.row].profileImage
+        cell!.studentNumber.text = self.datas[indexPath.row].studentNumber
+        cell!.name.text = self.datas[indexPath.row].name
+        cell!.title.text = self.datas[indexPath.row].title
+        cell!.detail.text = self.datas[indexPath.row].detail
         
-        return cell
+        cell!.setup()
+        cell!.selectionStyle = .none
+        
+        return cell ?? QuestionTableViewCell()
     }
 }
