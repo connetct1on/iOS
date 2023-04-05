2efda93e32c0db8129d082d93914f8f9f4acff1e
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-04-05 23:45:35 +0900

    ✨ :: 이니셜라이져 주석삭제

--- connect1on/connect1on/Model/Question.swift
+++ connect1on/connect1on/Model/Question.swift
@@ -8,17 +8,17 @@
 import UIKit
 
 struct Question {
-    var profileImage: UIImage?
-    var studentNumber: String?
-    var name: String?
-    var title: String?
-    var detail: String?
-//    
-//    init(profileImage: UIImage, studentNumber: String, name: String, title: String, detail: String) {
-//        self.profileImage = profileImage
-//        self.studentNumber = studentNumber
-//        self.name = name
-//        self.title = title
-//        self.detail = detail
-//    }
+    var profileImage = UIImage(named: "love")
+    var studentNumber: String = "1117"
+    var name: String = "최시훈"
+    var title: String = "화장실에서"
+    var detail: String = "똥 좀 싸도 되나요"
+    
+    init(profileImage: UIImage, studentNumber: String, name: String, title: String, detail: String) {
+        self.profileImage = profileImage
+        self.studentNumber = studentNumber
+        self.name = name
+        self.title = title
+        self.detail = detail
+    }
 }
